818840a89f838fccef33ef6b8c8c3e2f53fb4885
unknown <damreyrizont@gmail.com>
2022-07-18 20:04:37 +0800

    Final Submission

--- task.js
+++ task.js
@@ -1,7 +1,36 @@
 <script>
 
-var a = "abc";
-console.log(a);
+var wholeString = prompt();
+var dividedString = wholeString.split(" ");
+var longestWord = dividedString[0];
+
+// for getting the scores in each length
+for (var i = 1; i < dividedString.length; i++){
+    if (getScore(longestWord) < getScore(dividedString[i])){
+    	longestWord = dividedString[i];
+    } 
+    else if (getScore(longestWord) == getScore(dividedString[i])){
+    	longestWord = getScoreVowels(longestWord, dividedString[i]);
+   	}
+
+}
+console.log(longestWord.match(/[a-zA-Z0-9]/g).join(''));
+
+function getScore(passedWord){
+	var expression = /[a-zA-Z0-9]/g;
+    return passedWord.match(expression).length;
+}
+
+function getScoreVowels(currentLongest, passedWord){
+	var expression = /[aeiou]/g;
+    
+	if (currentLongest.match(expression).length > passedWord.match(expression).length){
+    	return currentLongest;
+    }
+    else {
+    	return passedWord;
+    }
+}
 
 
 </script
